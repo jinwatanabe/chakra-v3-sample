6237f4770ecee613137412dd38ced67c03493cb7
jinwatanabe <jin.watanabe.6g@gmail.com>
2024-11-06 21:56:47 +0900

    dialogをだす

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Button, DialogBackdrop, DialogTitle, HStack } from "@chakra-ui/react";
+import {
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTrigger,
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <>
+      <DialogRoot>
+        <HStack>
+          <DialogTrigger asChild>
+            <Button>Click me</Button>
+          </DialogTrigger>
+          <Button>Click me</Button>
+        </HStack>
+        <DialogBackdrop />
+        <DialogTrigger />
+        <DialogContent>
+          <DialogCloseTrigger />
+          <DialogHeader>
+            <DialogTitle />
+          </DialogHeader>
+          <DialogBody />
+          <DialogFooter />
+        </DialogContent>
+      </DialogRoot>
+    </>
   );
 }
 
